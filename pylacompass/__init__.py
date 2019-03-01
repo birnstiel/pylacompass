f3877b47d422a5ccefdf134ab3dd179299efeccc
birnstiel@me.com <birnstiel@me.com>
2019-03-01 21:58:27 +0100

    function missing in __init__

--- pylacompass/__init__.py
+++ pylacompass/__init__.py
@@ -1,5 +1,11 @@
 """Initialization file for the pylacompass module"""
-from .pylacompass import read_data, read_input, data_dict, read_hdf5_file, read_torqfile, convert_to_cgs
+from .pylacompass import read_data, \
+    read_input, \
+    data_dict, \
+    read_hdf5_file, \
+    read_torqfile,\
+    convert_to_cgs, \
+    get_snapshot_number
 from .plotting import twod_plot
 __all__ = [
     'read_data',
@@ -8,5 +14,6 @@ __all__ = [
     'read_hdf5_file',
     'read_torqfile',
     'convert_to_cgs',
-    'twod_plot'
+    'twod_plot',
+    'get_snapshot_number',
     ]
